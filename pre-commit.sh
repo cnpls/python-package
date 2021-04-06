d260879b4ce8fa8a30acd7882c1b972f5e406b5c
chris <14341145+cnpls@users.noreply.github.com>
2021-04-06 12:44:37 -0400

    Update pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,13 +1,13 @@
 echo "Running install check"
 rm -rf pre-commit-venv
 python -m venv pre-commit-venv
-pre-commit-venv/scripts/python.exe -m pip install --upgrade pip
-pre-commit-venv/scripts/python.exe -m pip install .
+pre-commit-venv/scripts/python.exe-m pip install --upgrade pip
+pre-commit-venv/scripts/pip install .
 
 echo "Running formatting, linting, tests"
-pre-commit-venv/scripts/python.exe -m pip install -r requirements-dev.txt
-pre-commit-venv/scripts/python.exe -m black package tests setup.py
-pre-commit-venv/scripts/python.exe -m flake8 package tests setup.py
+pre-commit-venv/scripts/pip install -r requirements.txt -r requirements-dev.txt
+pre-commit-venv/scripts/python.exe -m black skadoo tests setup.py
+pre-commit-venv/scripts/python.exe -m flake8 skadoo tests setup.py
 pre-commit-venv/scripts/python.exe -m pytest
 
 echo "Removing pre-commit venv"
