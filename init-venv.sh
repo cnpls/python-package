3b61f30689bf3d589aa8de3d7c964e1e8e40b771
chris <14341145+cnpls@users.noreply.github.com>
2021-04-12 20:43:15 -0400

    Update init-venv.sh

--- init-venv.sh
+++ init-venv.sh
@@ -2,3 +2,7 @@ rm -rf venv
 python -m venv venv
 venv/scripts/python.exe -m pip install --upgrade pip
 venv/scripts/pip install -r requirements.txt -r requirements-dev.txt
+
+# if using an IPython kernel
+# #venv/scripts/ipython kernel install --user --name=my-project
+# some environments would use IPython kernel install --user --name=my-project
